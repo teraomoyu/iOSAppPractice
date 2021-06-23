30e562d7e92a7f13029dd8a37937fa9699ebda34
teraomoyu <dmntc.5299@gmail.com>
2021-06-23 21:48:34 +0900

    Initial Commit

---
+++ FirstXcode/FirstXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstXcodeApp.swift
+//  FirstXcode
+//
+//  Created by Moyu Terao on 2021/06/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
